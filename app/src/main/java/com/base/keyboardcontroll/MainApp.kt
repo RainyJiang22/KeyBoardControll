0321c581e920d6962c4b9d3a169e784f1f6b08e8
jiangshiyu <3434481891@qq.com>
2022-07-15 14:17:19 +0800

    init commits

---
+++ app/src/main/java/com/base/keyboardcontroll/MainApp.kt
@@ -0,0 +1,14 @@
+package com.base.keyboardcontroll
+
+import android.app.Application
+
+/**
+ * @author jiangshiyu
+ * @date 2022/7/15
+ */
+class MainApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
